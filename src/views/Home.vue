54479da94d39274f0ac5cca4262eb3be5641f6f8
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2020-02-27 10:30:44 +0100

    Tatt i bruk litt lærdom fra en vue-ekspert

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="home">
 		<h1>Værsego'! Lag magi</h1>
+      <div class="WizardStep">
+          <div v-if="showQuestion1">
+              <div class="Quesion">Liker du jazz?</div>
+              <button class="Yes" @click="toggleQuestion1()">Ja</button>
+              <button class="No" @click="showQuestion1 = false">Nei</button>
+          </div>
+      </div>
   </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: 'Home'
+    name: 'Home',
+    data: () => ({
+        showQuestion1: true,
+    }),
+    methods: {
+        toggleQuestion1() {
+            this.showQuestion1 =
+        }
+    }
 }
 </script>
 
